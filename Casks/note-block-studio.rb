96522da701fad0fec4b17de745cff64c4acad12c
Prince527 <Prince527gaming@gmail.com>
2025-01-05 08:38:11 -0400

    Add files via upload

---
+++ Casks/note-block-studio.rb
@@ -0,0 +1,11 @@
+cask "note-block-studio" do
+  version "3.12.0-dev"
+  sha256 "fbdfc870f24c3a1ef615c0e9608d1d8d887dae227520e66abdb746036ad9b4db"
+
+  url "https://github.com/ForkPrince/tap/raw/c60a096fb2552904ec5eada157bcc8427eba13d9/Apps/Note%20Block%20Studio.dmg"
+  name "Note Block Studio"
+  desc "An open-source Minecraft music editor"
+  homepage "https://opennbs.org/"
+
+  app "Minecraft Note Block Studio.app"
+end
