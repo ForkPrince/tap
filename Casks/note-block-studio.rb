d886795b8e2777d402f398abfd9feabeddae40e3
Prince527 <Prince527gaming@gmail.com>
2025-01-05 08:43:19 -0400

    Update note-block-studio.rb

--- Casks/note-block-studio.rb
+++ Casks/note-block-studio.rb
@@ -1,6 +1,6 @@
 cask "note-block-studio" do
   version "3.12.0-dev"
-  sha256 "fbdfc870f24c3a1ef615c0e9608d1d8d887dae227520e66abdb746036ad9b4db"
+  sha256 "69f120829f794c26a7fc092a2987fee9119e7a2d22c38a1158926924bbc79e01"
 
   url "https://github.com/ForkPrince/tap/raw/c60a096fb2552904ec5eada157bcc8427eba13d9/Apps/Note%20Block%20Studio.dmg"
   name "Note Block Studio"
