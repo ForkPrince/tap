fe79533e1c1edc29d104a4453f1408af958b1be7
Prince527 <Prince527gaming@gmail.com>
2025-07-04 08:32:27 -0300

    Update relunatic-player.rb

--- Casks/relunatic-player.rb
+++ Casks/relunatic-player.rb
@@ -1,6 +1,6 @@
 cask "relunatic-player" do
   version "1.1.0"
-  sha256 "a6a72673c1c3c52677b8a464ba8bb2817b2b5c58bf3beca843e959dd47bd59d6"
+  sha256 "e8e2db3578952ec8293ab0af319bccf7a5afd7796a98d66adb521cc862fd80d7"
 
   url "https://github.com/Prince527GitHub/Re-Lunatic-Player/releases/download/v#{version}/Re.Lunatic.Player.dmg"
   name "Re:Lunatic Player"
