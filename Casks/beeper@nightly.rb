59578147e5ea25c1cdc482a79f61e4b214cee76a
Prince527 <Prince527gaming@gmail.com>
2025-07-16 13:11:43 -0300

    Fix beeper@nightly

--- Casks/beeper@nightly.rb
+++ Casks/beeper@nightly.rb
@@ -6,7 +6,7 @@ cask "beeper@nightly" do
   sha256 arm:   "1812818ace988d29998b982176168947c06bfaa3281222c6b072a57b2608331f",
          intel: "b90daf8e6952d8141a1c26a2a342e6854fb994243d81bf797568369d7571c5ae"
 
-  url "https://beeper-desktop.download.beeper.com/builds/Beeper-#{version}#{arch}-mac.zip"
+  url "https://beeper-desktop.download.beeper.com/builds/Beeper%20Nightly-#{version}#{arch}-mac.zip"
   name "Beeper"
   desc "Universal chat app powered by Matrix"
   homepage "https://www.beeper.com/"
