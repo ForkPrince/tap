55a6a56eab163fb2a750c0b359822a00d3f6d150
Prince527 <Prince527gaming@gmail.com>
2025-07-21 16:36:12 -0300

    Update peazip.rb

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -2,7 +2,8 @@ cask "peazip" do
   version "10.3.0"
   sha256 "265999996ad1ecfaaacad2a2ac0805200e6957790fb1bd039f0b0b4b7e439b4e"
 
-  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.aarch64.dmg"
+  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.aarch64.dmg",
+      verified: "github.com/peazip/PeaZip"
   name "PeaZip"
   desc "Free Zip / Unzip software and Rar file extractor."
   homepage "https://peazip.github.io"
