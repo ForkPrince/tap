4392d59621b47ec95cddb603a8bd7dc1ad28ac7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-30 04:04:28 +0000

    tablift 1.7

--- Casks/tablift.rb
+++ Casks/tablift.rb
@@ -1,6 +1,6 @@
 cask "tablift" do
-  version "1.6.1"
-  sha256 "0dbef2e690921fddb0e45ea7d7019e257b446344d6c8a34191219570b5e199fc"
+  version "1.7"
+  sha256 "baa5e3e206136d92b455f9440b3260966a8ce0320ea5d69e5f687babfda2f3b4"
 
   url "https://github.com/turtle-key/TabLift/releases/download/v#{version}/TabLift.dmg",
       verified: "github.com/turtle-key/TabLift/"
