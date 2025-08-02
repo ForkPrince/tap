84cf160846c751bf965b5e8e6d0a1193b6aa7f24
Prince527 <Prince527gaming@gmail.com>
2025-08-01 22:03:59 -0300

    Fix Geode

--- Casks/geode.rb
+++ Casks/geode.rb
@@ -2,7 +2,7 @@ cask "geode" do
   version "4.7.0"
   sha256 "5041ffcea55024b6b274487cab675efb0ca5c00f49d83e0a479c2ecfcf198ff4"
 
-  url "https://github.com/geode-sdk/geode/releases/download/v#{version}/geode-installer-v#{version}-mac.pkg ",
+  url "https://github.com/geode-sdk/geode/releases/download/v#{version}/geode-installer-v#{version}-mac.pkg",
       verified: "github.com/geode-sdk/geode/"
   name "Geode"
   desc "The ultimate Geometry Dash modding framework"
