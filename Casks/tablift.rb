26be70a3779d28a761e417480d2b985706ac1621
Prince527 <prince527@duck.com>
2025-08-06 01:05:39 -0300

    tablift 1.9 (#24)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/tablift.rb
+++ Casks/tablift.rb
@@ -1,6 +1,6 @@
 cask "tablift" do
-  version "1.8"
-  sha256 "99467c250847e61c0b8dac866369fe448dbcf141162195306a8c1f881938e1b8"
+  version "1.9"
+  sha256 "28cfa52b74f6b2a9ef4cb6f21cc9fe64c75d8dab403a3be761bc98b0e1cb5d0f"
 
   url "https://github.com/turtle-key/TabLift/releases/download/v#{version}/TabLift.dmg",
       verified: "github.com/turtle-key/TabLift/"
